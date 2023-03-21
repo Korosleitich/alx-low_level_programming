5b494953441efca650dab390a51e3d8d4591b380
Korosleitich <kiplangatkoros01@gmail.com>
2023-03-21 03:04:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <uistd.h>
+
+/**
+ * _putchar - writes che character c to stdout
+ * @c : the character to print
+ * Return : 1 On success
+ */
+
+int _puchar(char c)
+{       
+        return (write(1, &c, 1));
+}       
+~                                                                              
+~             
