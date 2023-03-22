d484a7d3fd0af2972830c77b3a70f950dea57412
Korosleitich <kiplangatkoros01@gmail.com>
2023-03-22 12:51:21 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * main - the function prints out _putchar
+ *
+ * Return: always returns zero
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
