4d3d3d9fcadec19b1e8faac907fc08994ba53cd1
Korosleitich <kiplangatkoros@gmail.com>
2023-03-30 00:07:25 -0700

    integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,11 +11,11 @@ void print_number(int n)
 unsigned int x;
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 n *= -1;
 }
 x = n;
 if (x / 10)
 print_number(x / 10);
-putchar(x % 10 + '0');
+_putchar(x % 10 + '0');
 }
