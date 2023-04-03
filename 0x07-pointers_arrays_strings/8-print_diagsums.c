ef902fb6a4b2af155ef01e400ed04056f8cb1b08
Korosleitich <kiplangatkoros@gmail.com>
2023-04-03 10:36:08 -0700

    Prints sum of two diagonals of a sqaure matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - displays diagonals
+* @a: matrix
+* @size: matrix size
+*/
+
+void print_diagsums(int *a, int size)
+{
+int diagonal_1 = 0;
+int diagonal_2 = 0;
+int row_1, q;
+for (row_1 = 0; row_1 < size; row_1++)
+{
+q = (row_1 * size) + row_1;
+diagonal_1 += a[q];
+}
+for (row_1 = 1; row_1 <= size; row_1++)
+{
+q = (row_1 * size) - row_1;
+diagonal_2 += a[q];
+}
+printf("%d, %d\n", diagonal_1, diagonal_2);
+}
