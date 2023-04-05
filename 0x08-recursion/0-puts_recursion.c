eed55668e7fe3fecd152a3a17888c9ad84adae2b
Korosleitich <kiplangatkoros@gmail.com>
2023-04-05 05:18:13 -0700

    prints a string and a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_puts_recursion - prints a string
+  *@s: pointer
+  *Return: void, meaning for an empty value
+  */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+}
