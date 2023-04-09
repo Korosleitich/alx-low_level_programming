b89e1d7e437613f9bbc07ee91c7f63afa64196fd
Korosleitich <kiplangatkoros@gmail.com>
2023-04-09 11:42:56 -0700

    picky

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc:  arguments
+ * @argv: Array of strings
+ *
+ * Return: Always 0  on Success
+ */
+int main(int argc, char **argv)
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
