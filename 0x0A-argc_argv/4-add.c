f5da1a7a5f8ea1b8d5f74f7c930c94ea927ec677
Korosleitich <kiplangatkoros@gmail.com>
2023-04-09 13:30:39 -0700

    adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * isInteger - checks if s is an integer
  * @s: string to be checked
  * Return: 0 or 1
  */
+
 int isInteger(const char *s)
 {
-int num = 0;
-if (s[num] == '-' || s[num] == '+')
-num++;
+int num;
+num = 0;
 while (s[num] != '\0')
 {
-if (s[num] < '0' || s[num] > '9')
-return (1);
-num++;
+	if (s[num] < '0' || s[num] > '9')
+		return (1);
+	num++;
 }
 return (0);
 }
@@ -23,12 +22,13 @@ return (0);
 /**
  * main - adds positive numbers
  * @argc: int seen
- * @argv: arguments
+ * @argv: list here
  * Return: 0
  */
 int main(int argc, char const *argv[])
 {
-int addition = 0;
+int addition;
+addition = 0;
 while (--argc)
 {
 if (isInteger(argv[argc]))
@@ -36,13 +36,7 @@ if (isInteger(argv[argc]))
 printf("Error\n");
 return (1);
 }
-int num = atoi(argv[argc]);
-if (num < 0)
-{
-printf("Error\n");
-return (1);
-}
-addition += num;
+addition += atoi(argv[argc]);
 }
 printf("%i\n", addition);
 return (0);
