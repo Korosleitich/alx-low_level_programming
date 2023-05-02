f5f22a999bec0922d289aafccafdcd4c72a07e1b
Korosleitich <kiplangatkoros@gmail.com>
2023-05-02 13:23:42 -0700

    frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
+ *
+ * Return: address of the node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+ listint_t *function = head;
+ listint_t *koros = head;
+
+ if (!head)
+ return (NULL);
+
+ while (function && koros && koros->next)
+ {
+ koros = koros->next->next;
+ function = function->next;
+ if (koros == function)
+ {
+ function = head;
+ while (function != koros)
+ {
+ function = function->next;
+ koros = koros->next;
+ }
+ return (koros);
+ }
+ }
+
+ return (NULL);
+}
